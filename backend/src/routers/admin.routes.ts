cff64fd348721d498026bae40200546c7396d036
paragkoche <paragkoche0@gmail.com>
2024-01-03 21:23:58 +0530

    [BACKEND] add admin routes

--- backend/src/routers/admin.routes.ts
+++ backend/src/routers/admin.routes.ts
@@ -4,7 +4,11 @@ import { Router } from "express";
 
 const router = Router();
 router.get("/", AdminToken, HomeData);
+// auth
 router.post("/admin-login");
 router.post("/admin-signing");
-
+// blog
+router.post("/admin-add-blog");
+router.delete("/admin-delete-blog");
+router.put("/admin-update-blog");
 export default router;
