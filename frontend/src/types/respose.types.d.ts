586609eeecda3f6fb5dd939f6a589322f58c01a2
paragkoche <paragkoche0@gmail.com>
2024-02-04 10:11:20 +0530

    update types

--- frontend/src/types/respose.types.d.ts
+++ frontend/src/types/respose.types.d.ts
@@ -1,4 +1,58 @@
-export type AdminHomeDataResType = {
-  message: string;
-  data: {};
-};
+import {
+  AdminSchemaType,
+  BlogSchemaType,
+  UserSchemeType,
+} from "./schema.types";
+
+export interface AdminHomeDataResponseType {
+  200: {
+    message: string;
+    data: {
+      blogs: BlogSchemaType[];
+      subs: UserSchemeType[];
+    };
+  };
+  500: {
+    message: string;
+  };
+}
+export interface AdminSignInResponseType {
+  402: { message: string; label: string }[];
+  401: { message: string };
+  200: { message: string; data: AdminSchemaType };
+  500: { message: string };
+}
+
+export interface AdminLoginResponseType {
+  402: { message: string; label: string }[];
+  401: { message: string };
+  200: { message: string; data: AdminSchemaType };
+  500: { message: string };
+}
+export interface AdminUpdateResponseType {
+  401: {
+    message: string;
+  };
+  402: {
+    message: string;
+    label: string;
+  }[];
+  200: {
+    message: string;
+  };
+  500: {
+    message: string;
+  };
+}
+
+export interface AdminDeleteResponseType {
+  401: {
+    message: string;
+  };
+  200: {
+    message: string;
+  };
+  500: {
+    message: string;
+  };
+}
