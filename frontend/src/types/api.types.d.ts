132df3e4dd9905a0924c08468f516151ec744dc7
paragkoche <paragkoche0@gmail.com>
2024-02-05 21:44:33 +0530

    update

--- frontend/src/types/api.types.d.ts
+++ frontend/src/types/api.types.d.ts
@@ -1,7 +1,13 @@
-import { Axios } from "axios";
+import { Axios, Axios } from "axios";
 
 export type AdminLoginApiFunction = (data: {
   emailId?: string;
   password: string;
   username?: string;
+}) => Promise<Axios>;
+export type AdminSingInApiFunction = (data: {
+  emailId: string;
+  password: string;
+  username: string;
+  bio?: string;
 }) => Promise<Axios>;
